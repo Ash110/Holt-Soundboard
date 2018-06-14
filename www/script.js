45213228721de8db08abe142fbbf1193a4d2ff2c
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2018-06-14 11:28:46 +0530

    3

--- www/script.js
+++ www/script.js
@@ -1,2 +1,7 @@
 $(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $(".goback2").click(function(){
+        window.location.href ="index.html";
+    });
 });
