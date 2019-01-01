004913c64ac8bfcf809483a7aa57d5c36c932a91
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2019-01-01 23:28:05 +0530

    `

--- www/script.js
+++ www/script.js
@@ -73,7 +73,25 @@ $(document).ready(function() {
         var fileName=String("newSounds/"+texto+".ogg");
         currentAudio.setAttribute('src', fileName);
         currentAudio.setAttribute('autoplay', 'autoplay');
-        currentAudio.play();        
+        currentAudio.play();
+        var options = {
+  message: 'share this', // not supported on some apps (Facebook, Instagram)
+  subject: 'the subject', // fi. for email
+  files: [fileName], // an array of filenames either locally or remotely
+  url: 'https://www.website.com/foo/#bar?a=b',
+  chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title,
+};
+
+var onSuccess = function(result) {
+  alert("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+  alert("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
+
+var onError = function(msg) {
+  alert("Sharing failed with message: " + msg);
+};
+
+window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
     });
     $(".holt-chat").click(function(){
         var url = "https://holt-chat.firebaseapp.com";
