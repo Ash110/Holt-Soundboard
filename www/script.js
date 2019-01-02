2b3ca125da2897a741c945f001d8fabb5b080618
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2019-01-02 08:26:49 +0530

    `

--- www/script.js
+++ www/script.js
@@ -26,7 +26,6 @@ var options = {
   files: ['', ''], // an array of filenames either locally or remotely
   url: 'https://www.website.com/foo/#bar?a=b',
   chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title,
-  appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
 };
 
 var onSuccess = function(result) {
