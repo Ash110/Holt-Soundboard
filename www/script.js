c379ab65d1a1104df5cb67a90de77a5c2d2e4ac7
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2019-01-02 08:07:20 +0530

    `

--- www/script.js
+++ www/script.js
@@ -4,6 +4,41 @@ function makeEverythingAppear(boxes)
         boxes[i].style.display="block";
 }
 $(document).ready(function() {
+    var options = {
+  message: 'share this', // not supported on some apps (Facebook, Instagram)
+  subject: 'the subject', // fi. for email
+  files: ['', ''], // an array of filenames either locally or remotely
+  url: 'https://www.website.com/foo/#bar?a=b',
+  chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title,
+};
+
+var onSuccess = function(result) {
+  console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+  console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
+
+var onError = function(msg) {
+  console.log("Sharing failed with message: " + msg);
+};
+var options = {
+  message: 'share this', // not supported on some apps (Facebook, Instagram)
+  subject: 'the subject', // fi. for email
+  files: ['', ''], // an array of filenames either locally or remotely
+  url: 'https://www.website.com/foo/#bar?a=b',
+  chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title,
+  appPackageName: 'com.apple.social.facebook' // Android only, you can provide id of the App you want to share with
+};
+
+var onSuccess = function(result) {
+  console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+  console.log("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
+
+var onError = function(msg) {
+  console.log("Sharing failed with message: " + msg);
+};
+
+window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
     $(".col-4").addClass("jackInTheBox");
 	var search=0;
 	var mywindow = $(window);
